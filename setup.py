586dfa9f338fea3272dc963e131ab39f0f5dc312
Jonathan Pinto Barbosa <jonathanpbarbosa@gmail.com>
2017-07-27 14:51:01 -0300

    Add python setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='stormtrooper',
+      version='0.0.1',
+      description='Report trooper status and geolocation using homomorphic encryption',
+      url='bitbucket.com',
+      author='Jonathan Pinto Barbosa',
+      author_email='jonathanpbarbosa@gmail.com',
+      license='MIT',
+      packages=['stormtrooper'],
+      install_requires=[],
+      zip_safe=False)
