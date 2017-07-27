0e8e30550936a15fb445172ea41b5971b1c4f0ce
Jonathan Pinto Barbosa <jonathanpbarbosa@gmail.com>
2017-07-27 14:49:17 -0300

    Test known values in split_into_integers

---
+++ tests/unit/test_stormtrooper.py
@@ -0,0 +1,14 @@
+import stormtrooper.stormtrooper as stormtrooper
+import unittest
+
+
+class KnowValues(unittest.TestCase):
+    known_coordinates = ((2.4, [2, 4]),
+                    (42.91, [42, 91]),
+                    (-22.456, [-22, 456]))
+
+    def test_split_into_int(self):
+        """split_into_integer should give known result with known input"""
+        for coordinate, intergers in self.known_coordinates:
+            result = stormtrooper.split_into_integers(coordinate)
+            self.assertEqual(intergers, result)
