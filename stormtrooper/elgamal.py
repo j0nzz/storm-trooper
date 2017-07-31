03a16e733db76523c2461b7a4e458d23f45e0056
Jonathan Pinto Barbosa <jonathanpbarbosa@gmail.com>
2017-07-31 13:24:58 -0300

    Add ElGamal encryption

--- stormtrooper/elgamal.py
+++ stormtrooper/elgamal.py
@@ -6,7 +6,10 @@ from Crypto.Hash import SHA
 import logging
 
 
-def key_construct(components):
+SCOPES = {'pub': ['p', 'g', 'y'], 'priv': ['x']}
+
+
+def construct(pub_dict, priv_dict):
     """Construct ElGamal object key based on given components
 
     :components: tuple ElGamal components usually p, g, y and x. Where x is
@@ -14,7 +17,10 @@ def key_construct(components):
     :returns: Crypto.PublicKey.ElGamal.ElGamalobj
 
     """
-    return ElGamal.construct(components)
+    return ElGamal.construct((pub_dict['p'],
+                          pub_dict['g'],
+                          pub_dict['y'],
+                          priv_dict['x']))
 
 
 def key_gen(size=1024):
@@ -40,3 +46,29 @@ def sign(plaintext, key):
         if GCD(k, key.p - 1) == 1:
             break
     return key.sign(h, k)
+
+
+def encrypt(lat, lon, key):
+    """TODO: Docstring for encrypt.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    while True:
+        k = random.StrongRandom().randint(1, key.p-1)
+        if GCD(k, key.p-1) == 1:
+            break
+    return key.encrypt(lat, k)
+
+
+def decrypt(enc_lat, enc_lon, key):
+    """TODO: Docstring for decrypt.
+
+    :enc_lat: TODO
+    :enc_lon: TODO
+    :key: TODO
+    :returns: TODO
+
+    """
+    return key.decrypt(enc_lat)
--- stormtrooper/stormtrooper.py
+++ stormtrooper/stormtrooper.py
@@ -26,14 +26,35 @@ def report(args):
     logging.info(position, troops)
 
 
-def generate(args):
-    """Generate ElGamal key pair"""
-    key = elgamal.key_gen()
-    scopes = {'pub': ['p', 'g', 'y', 'x'], 'priv': ['x']}
-    for scope, components in scopes.items():
-        component_dict = {k: getattr(key, k) for k in components}
-        with open('.'.join([args.output, scope]), 'w', encoding='utf8') as of:
-            json.dump(component_dict, of)
+def encrypt(args):
+    """Encrypt data using ElGamal
+
+    :args: parsed args
+    :returns: tuple with 2 items
+
+    """
+    with open(args.public_key_path, 'r', encoding='utf8') as f_pub:
+        public_dict = json.load(f_pub)
+    with open(args.private_key_path, 'r', encoding='utf8') as f_priv:
+        private_dict = json.load(f_priv)
+    key = elgamal.construct(public_dict, private_dict)
+    enc_geo = elgamal.encrypt(args.latitude, args.longitude, key)
+    print(enc_geo)
+
+    print('>', elgamal.decrypt(enc_geo, enc_geo, key))
+
+
+class Key(object):
+
+    """Key args handler"""
+
+    def generate(self, args):
+        """Generate ElGamal key pair"""
+        key = elgamal.key_gen()
+        for scope, components in elgamal.SCOPES.items():
+            component_dict = {k: getattr(key, k) for k in components}
+            with open('.'.join([args.output, scope]), 'w', encoding='utf8') as of:
+                json.dump(component_dict, of)
 
 
 def config_logging():
@@ -66,18 +87,30 @@ def main():
     parser_key_gen = subparsers_key.add_parser('gen',
                                                help='Generate a new key')
     parser_key_gen.add_argument('-o', '--output', default='id_elgamal')
-    parser_key_gen.set_defaults(func=generate)
+    parser_key_gen.set_defaults(func=Key.generate)
+    # parser for "encrypt" command
+    parser_encrypt = subparsers.add_parser('encrypt',
+                                       help='Encrypt message')
+    parser_encrypt.add_argument('latitude', help='Latitude to be encrypted',
+                                type=int)
+    parser_encrypt.add_argument('longitude', help='Longitude to be encrypted',
+                                type=int)
+    parser_encrypt.add_argument('public_key_path',
+                                help='ElGamal path to public key')
+    parser_encrypt.add_argument('private_key_path',
+                                help='ElGamal path to private key')
+    parser_encrypt.set_defaults(func=encrypt)
     # parse
     args = parser.parse_args()
 
     if args.verbose:
         config_logging()
 
-    try:
-        args.func(args)
-    except:
-        parser.print_help()
-        sys.exit(0)
+    #try:
+    args.func(args)
+    #except:
+    #    parser.print_help()
+    #    sys.exit(0)
 
 
 if __name__ == "__main__":
